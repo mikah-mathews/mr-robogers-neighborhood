a3450adbc46e9804c07cf38076b7793b242ada47
Mikah Mathews <mikah@outofband.org>
2020-04-03 11:16:15 -0700

    Add working counter - loop function will count up to user number then display array of numbers resulting from counting up to user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 // back logic
 var printedArray = [];
 var startCount = 0;
-function counter(numberInput) {
-  for(i = 0; i < numberInput; i++) {
-    startCount = startCount + 1;
-    printedArray.push(startCount);
-    console.log(startCount);
-    console.log(printedArray);
-    return startCount;
+var counterLoop = function(numberInput) {
+  for(i = 0; i <= numberInput; i++) {
+    if(startCount < numberInput) {
+      console.log("This is the number input " + numberInput);
+      console.log("This is starting the loop");
+      console.log(printedArray + " = array contents");
+      startCount = startCount + 1;
+      printedArray.push(startCount);
+    } 
   }
 }
 
@@ -17,34 +19,38 @@ function counter(numberInput) {
 
 //front end logic
 $(document).ready(function() {
-  
-  
-  
-  
+// Variable placement is important - wasn't working until variables were placed inside submit function
+
+  // when start button is pressed, show input form
   $("#startButton").click(function() {
     event.preventDefault();
     $(".robot-start").hide();
     $(".robot-input").show();
   });
 
-  $("#submit").click(function() {
+  // when submit button is pressed, show results
+  $("#submit").click(function() { 
     event.preventDefault();
     $(".robot-input").hide();
     $(".output").show();
     var userInput = $("#number").val();
     var numInput = parseInt(userInput);
-    var countOutput = counter(numInput)
+    var countOutput = counterLoop(numInput)
     console.log(userInput);
     console.log(numInput);
-    console.log(countOutput);
-    $(".output").text(countOutput);
+    console.log(printedArray);
+    countOutput;
+    $("#array-output").text(printedArray);
     
   });
 
+  // When restart button is pressed, switch to input form again
   $("#restartButton").click(function() {
     event.preventDefault();
     $(".output").hide();
     $(".robot-input").show();
+    printedArray = [];
+    startCount = 0;
   });
   
 });
